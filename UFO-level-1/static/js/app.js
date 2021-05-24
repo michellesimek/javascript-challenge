4aae319c14ef6f2c38600d4b6e37c86d8b82657c
Michelle Simek <michellesimek@Michelles-MacBook-Pro.local>
2021-05-24 19:09:46 -0400

    table shows when pages loads, but is currently not filtering. working file uploaded

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,10 +4,16 @@ var tableData = data;
 // create a new variable selecting the table body in the html
 let tbody = d3.select("tbody");
 
+// Grab the datetime value from the filter
+let inputField = d3.select("#datetime");
+
+// create a new variable for the filter date button
+let button = d3.select("#filter-btn");
+
 // When the page loads, it needs to display the table
 // use forEach to loop through the dataset and append rows for each element
 function buildTable(data) {
-    tableData.forEach(function(data) {
+    data.forEach(function(data) {
     let nextrow = tbody.append("tr");
     nextrow.append("td").text(data["datetime"]).style("text-align", "center");
     nextrow.append("td").text(data["city"]);
@@ -18,37 +24,43 @@ function buildTable(data) {
     nextrow.append("td").text(data["comments"]);
 })};
 
-
 // create a function that is triggered when the button is clicked
 function handleClick() {
-    // create a new variable for the filter date button
-    let button = d3.select("#button");
 
-    // Grab the datetime value from the filter
+    // clear/overwrite previous data 
+    list.html("");
+    // var filteredData = tableData;
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // // Grab the datetime value from the filter
     let inputField = d3.select("#datetime");
 
-    // get the value of the input element 
-    let inputValue = inputElement.property("value");
-    let filteredData = tableData.filter(function(tbody) {
-        if(tableData["datetime"] === inputValue) {
-           return true;
-        } else {
-            return false;
-        }
-    })
+    // Grab the datetime value from the filter
+    let inputValue = inputField.property("value");
+
+    // grab all the table data and set to filteredData
+    // if (inputValue) {
+    let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    // };
+    
+    // let filteredData = tableData.filter(function(tbody) {
+    //     if(tableData["datetime"] === inputValue) {
+    //        return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
+
+    console.log(filteredData);
     buildTable(filteredData);
 };
 
-// filteredData.forEach(function(tableData) {
-//     let nextrow = tbody.append("tr");
-//     nextrow.append("td").text(elt["datetime"]).style("text-align", "center");
-//     nextrow.append("td").text(elt["city"]);
-//     nextrow.append("td").text(elt["state"]);
-//     nextrow.append("td").text(elt["country"]);
-//     nextrow.append("td").text(elt["shape"]);
-//     nextrow.append("td").text(elt["durationMinutes"]);
-//     nextrow.append("td").text(elt["comments"]);
-// });
-
 // Build the table when the page loads
 buildTable(tableData);
+
+
+
+// run handleClick function when filter button is clicked
+button.on("click", handleClick);
